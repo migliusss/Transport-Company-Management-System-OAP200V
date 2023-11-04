cca50c472317b03cac2afccf5eab715a32c5ef59
Migle <miglea97@gmail.com>
2023-11-04 15:19:33 +0100

    Created a maven project

---
+++ TransportCompanyManagementSystem/src/main/java/com/oap200vGroupNumberFive/TransportCompanyManagementSystem/App.java
@@ -0,0 +1,13 @@
+package com.oap200vGroupNumberFive.TransportCompanyManagementSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
